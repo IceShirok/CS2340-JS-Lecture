a9683a75d88997524a6fa1ea4284dd6c645f3ada
IceShirok <dongsusanna@gmail.com>
2014-07-03 08:02:53 -0400

    Modified JS example

--- examples/js/aGame.js
+++ examples/js/aGame.js
@@ -3,14 +3,15 @@ var startWidth = 0;
 var startHeight = 0;
 var squareSize = 100;
 var offset = squareSize/2;
-randomize();
+$(document).ready(function() {
+    randomize();
+});
 
 function randomize() {
     startWidth = Math.floor((Math.random() * $(window).width()) + 1);
     startHeight = Math.floor((Math.random() * $(window).height()) + 1);
     $("#message").text("Find the hidden block!");
     // document.getElementById("message").innerText = tellIfClose("Find the hidden block!");
-    //$("#random").text(" Testing pageX: " + startWidth + ", pageY: " + startHeight );
 }
 
 function tellIfClose(x, y) {
@@ -19,7 +20,6 @@ function tellIfClose(x, y) {
     var isClose = Math.sqrt(xDis*xDis + yDis*yDis);
     
     $("#test").text(x + " " + y);
-    //document.getElementById("test").innerText = tellIfClose(x + " " + y);
     if (isClose > 550) {
         return "Cold!";
     } else if (isClose > 200) {
@@ -35,13 +35,11 @@ function tellIfClose(x, y) {
 
 $(document).on("mousemove", function(event) {
     $("#howClose").text(tellIfClose(event.pageX, event.pageY));
-    // document.getElementById("howClose").innerText = tellIfClose(event.pageX, event.pageY);
 });
 
 $(document).on("click", function(event) {
     if(event.pageX>startWidth && event.pageX<startWidth+squareSize
             && event.pageY>startHeight && event.pageY<startHeight+squareSize) {
         $("#message").text("Congratulations! You've found the hidden block!");
-        // document.getElementById("message").innerText = "Congratulations! You've found the hidden block!";
     }
 });
